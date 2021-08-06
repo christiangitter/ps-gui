5c39ab3c3ab8bd206f41f1dcbb5460a6fd78218d
Christian Gitter <c.gitter@heurich.de>
2021-08-06 08:17:18 +0200

    define functions

--- script.ps1
+++ script.ps1
@@ -1,5 +1,7 @@
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
+#####################START BUILD THE GUI##############################
+######################################################################
 #Settings for the Window
 $psGui = New-Object system.Windows.Forms.Form
 $psGui.ClientSize = New-Object System.Drawing.Point(305, 400)
@@ -39,5 +41,28 @@ $LabelTxt.location = New-Object System.Drawing.Point(28, 292)
 $LabelTxt.Font = New-Object System.Drawing.Font('Microsoft Sans Serif', 12)
 #set all the components visible
 $psGui.controls.AddRange(@($ButtonMsgBx, $ButtonLblTxt, $ButtonPthDlg, $LabelTxt))
+#####################END BUILD THE GUI################################
+######################################################################
+function showMsgBox() {
+    [System.Windows.Forms.MessageBox]::Show("Pretty nice Text","MY MESSAGEBOX",0)
+}
+function setLblText(){
+    $LabelTxt.Text += "I LOVE MY LABEL" + "`n"
+}
+function showPath(){
+    $OrdnerWahl = New-Object System.Windows.Forms.FolderBrowserDialog        
+    $OrdnerWahl.ShowNewFolderButton = $false  
+	$OrdnerWahl.ShowDialog() 
+    if ($OrdnerWahl.SelectedPath -ne "")            
+    {
+        $ordnerpfad = $OrdnerWahl.SelectedPath
+		$LabelTxt.Text += "Pfad: " + $ordnerpfad + "`n"
+    }
+}
+
+$ButtonMsgBx.Add_Click({showMsgBox})
+$ButtonLblTxt.Add_Click({setLblText})
+$ButtonPthDlg.Add_Click({showPath})
+
 #prompt all the components
 [void]$psGui.ShowDialog()
